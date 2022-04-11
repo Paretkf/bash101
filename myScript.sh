2db478d782a20394f88d2a3647336f202afe9629
Paretkf <paretk7@gmail.com>
2022-04-11 11:16:56 +0700

    :rocket: bash 101

---
+++ myScript.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# from : https://www.youtube.com/watch?v=v-F3YLd6oMw&t=131s&ab_channel=TraversyMedia
+
+# Echo CMD
+echo Hello bash!
+
+# VARIABLES
+# Uppercase
+# Letters numbers underscores
+
+NAME="paretkf"
+
+echo "My name is $NAME"
+# My name is paretkf
+
+echo "My name is ${NAME}"
+# My name is paretkf
+
+
+# User input
+# read p "Enter your name : " NAME
